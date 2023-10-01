aff1624d641af133b5dd72fb02114a182fc62e0e
owolfdev <owolfdev@gmail.com>
2023-10-01 09:31:25 +0700

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/ui/themeProvider";
 import { SiteHeader } from "@/components/nav/siteHeader";
-import { headers } from "next/headers";
-import { usePathname } from "next/navigation";
 
-// export const metadata: Metadata = {
-//   title: "Supabase Auth",
-//   description:
-//     "Supabase Auth Template with Next.js 13. Cookie-based authentication offers a secure and streamlined method for managing user sessions",
-// };
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
